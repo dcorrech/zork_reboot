2362f0028d779456bd85563293c06fd350f9f14b
Jeff <jeffrey.miiller@gmail.com>
2019-10-13 23:47:52 -0700

    Add TestUtils.

---
+++ HaskellAdventure-master/TestUtils.hs
@@ -0,0 +1,62 @@
+module TestUtils where
+
+import NaturalLanguageLexer
+import NaturalLanguageParser
+
+dummyTokenVerb :: Token
+dummyTokenVerb = (TokenVerb "verb1" ["verb1", "synonym"])
+
+secondDummyTokenVerb :: Token
+secondDummyTokenVerb = (TokenVerb "verb2" ["verb2", "synonym"])
+
+dummyTokenNoun :: Token
+dummyTokenNoun = (TokenNoun "noun1" ["noun1", "synonym"])
+
+equalTokenVerb :: Token
+equalTokenVerb = (TokenVerb "equal1" ["equal1", "synonym"])
+
+equalTokenNoun :: Token
+equalTokenNoun = (TokenNoun "equal1" ["equal1", "synonym"])
+
+dummyTokenVerbMatch :: TokenMatch
+dummyTokenVerbMatch = (TokenMatch "verb1" [dummyTokenVerb])
+
+dummyTokenNounMatch :: TokenMatch
+dummyTokenNounMatch = (TokenMatch "noun1" [dummyTokenNoun])
+
+equivalentTokenVerbMatch :: TokenMatch
+equivalentTokenVerbMatch = (TokenMatch "equal1" [equalTokenNoun])
+
+equivalentTokenNounMatch :: TokenMatch
+equivalentTokenNounMatch = (TokenMatch "equal1" [equalTokenVerb])
+
+equivalentTokenJoinedMatch :: TokenMatch
+equivalentTokenJoinedMatch = (TokenMatch "equal1" [equalTokenVerb, equalTokenNoun])
+
+possibleTokens :: [Token]
+possibleTokens = [dummyTokenVerb, dummyTokenNoun, secondDummyTokenVerb, equalTokenVerb, equalTokenNoun]
+
+dummyListOfTokens :: [Token]
+dummyListOfTokens = [dummyTokenVerb,
+                     dummyTokenNoun,
+                     (TokenVerb "verb2" ["verb2", "synonym"]),
+                     (TokenVerb "verb3" ["verb3", "synonym"]),
+                     (TokenNoun "noun2" ["noun2", "synonym"])]
+
+verbsInDummyList :: [Token]
+verbsInDummyList = [dummyTokenVerb,
+                    (TokenVerb "verb2" ["verb2", "synonym"]),
+                    (TokenVerb "verb3" ["verb3", "synonym"])]
+
+nounsInDummyList :: [Token]
+nounsInDummyList = [dummyTokenNoun,
+                    (TokenNoun "noun2" ["noun2", "synonym"])]
+
+allPossibleSimpleSentencesFromVerbNounDummyLists :: [Sentence]
+allPossibleSimpleSentencesFromVerbNounDummyLists = [SimpleSentence (TokenVerb "verb1" ["verb1","synonym"]) (TokenNoun "noun1" ["noun1","synonym"]),
+                                                    SimpleSentence (TokenVerb "verb1" ["verb1","synonym"]) (TokenNoun "noun2" ["noun2","synonym"]),
+                                                    SimpleSentence (TokenVerb "verb2" ["verb2","synonym"]) (TokenNoun "noun1" ["noun1","synonym"]),
+                                                    SimpleSentence (TokenVerb "verb2" ["verb2","synonym"]) (TokenNoun "noun2" ["noun2","synonym"]),
+                                                    SimpleSentence (TokenVerb "verb3" ["verb3","synonym"]) (TokenNoun "noun1" ["noun1","synonym"]),
+                                                    SimpleSentence (TokenVerb "verb3" ["verb3","synonym"]) (TokenNoun "noun2" ["noun2","synonym"])]
+
