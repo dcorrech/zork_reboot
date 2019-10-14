d25e78fd9c980ccc467ad1427640f13fb3de9ce0
Jeff <jeffrey.miiller@gmail.com>
2019-10-14 13:15:21 -0700

    Add rename possibleTokens test util to dummyPossibleTokens

--- HaskellAdventure-master/TestUtils.hs
+++ HaskellAdventure-master/TestUtils.hs
@@ -33,8 +33,8 @@ equivalentTokenNounMatch = (TokenMatch "equal1" [equalTokenVerb])
 equivalentTokenJoinedMatch :: TokenMatch
 equivalentTokenJoinedMatch = (TokenMatch "equal1" [equalTokenVerb, equalTokenNoun])
 
-possibleTokens :: [Token]
-possibleTokens = [dummyTokenVerb, dummyTokenNoun, secondDummyTokenVerb, equalTokenVerb, equalTokenNoun]
+dummyPossibleTokens :: [Token]
+dummyPossibleTokens = [dummyTokenVerb, dummyTokenNoun, secondDummyTokenVerb, equalTokenVerb, equalTokenNoun]
 
 dummyListOfTokens :: [Token]
 dummyListOfTokens = [dummyTokenVerb,
